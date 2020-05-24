4dc211315e128e147fee7f54578fe80b82f46ec1
tony-tsx <tony.js@zoho.eu>
2020-05-24 15:22:48 -0300

    CookieX emitter

---
+++ __TEST__/emitter.spec.ts
@@ -0,0 +1,71 @@
+import Emitter from '../src'
+
+describe( 'emitter tests suits', () => {
+  it( 'counter', () => {
+    interface Events { increment( counter: number ): void }
+
+    const emitter = new Emitter<Events>()
+  
+    let counter = 0
+
+    emitter.on( 'increment', () => counter++ )
+
+    while( counter < 10 ) emitter.emit( 'increment', counter )
+
+    expect( counter ).toEqual( 10 )
+
+    emitter.once( 'increment', counter => {
+      expect( counter + 2 ).toEqual( 12 )
+    } )
+
+    expect( emitter.count( 'increment' ) ).toEqual( 2 )
+
+    emitter.emit( 'increment', counter )
+
+    expect( emitter.count( 'increment' ) ).toEqual( 1 )
+
+    expect( counter ).toEqual( 11 )
+
+    emitter.off( 'increment' )
+
+    expect( emitter.count( 'increment' ) ).toEqual( 0 )
+
+  } )
+
+  it( 'multiple listeners with counter', () => {
+    interface Events {
+      increment: () => void
+      decrement: () => void
+    }
+    const emitter = new Emitter<Events>()
+
+    let counter = 0
+
+    const unlistener = emitter.on( {
+      increment: () => { counter++ },
+      decrement: [ () => { counter-- }, () => {} ]
+    } )
+
+    expect( emitter.count( 'increment' ) ).toEqual( 1 )
+    expect( emitter.count( 'decrement' ) ).toEqual( 2 )
+
+    emitter.emit( 'increment' )
+    emitter.emit( 'increment' )
+
+    expect( counter ).toEqual( 2 )
+
+    emitter.emit( 'decrement' )
+
+    expect( counter ).toEqual( 1 )
+
+    unlistener()
+
+    expect( emitter.count( 'increment' ) ).toEqual( 0 )
+    expect( emitter.count( 'decrement' ) ).toEqual( 0 )
+
+    emitter.emit( 'increment' )
+
+    expect( counter ).toEqual( 1 )
+
+  } )
+} )
---
+++ src/index.ts
@@ -0,0 +1,112 @@
+const entries: {
+  <O>( object: O ): [ [ keyof O, O[keyof O] ] ]
+} = ( object: any ) => {
+  const keys: ( string | symbol )[] = Object.keys( object )
+  keys.push( ...Object.getOwnPropertySymbols( object ) )
+
+  return keys.map( ( key ) => [ key, object[key] ] ) as any
+}
+
+const forEachUnlisteners = ( unlistener: () => void ) => unlistener()
+
+class Emitter<E> {
+
+  private __events: Emitter.Events<E> = {}
+
+  public count = <K extends keyof E>( event: K ) => this.listeners( event ).length
+
+  public listeners = <K extends keyof E>( event: K ): Emitter.ListenerAssert<E, K>[] => {
+    if ( !this.__events[event] ) return this.__events[event] = []
+    return this.__events[event] ?? []
+  }
+
+  public once: Emitter.Once<E> = <K extends keyof E>(
+    event: K | Emitter.ObjectListeners<E>,
+    listener?: Emitter.ListenerAssert<E, K>,
+    ...listeners: Emitter.ListenerAssert<E, K>[]
+  ) => {
+    if ( typeof event === 'object' )
+      entries( event )
+        .map( ( [ event, listeners ] ) => {
+          if ( Array.isArray( listeners ) )
+            if ( listeners.length ) return this.once( event, listeners[0], ...listeners.slice( 1 ) )
+            else return () => {}
+          return this.once( event, listeners as Emitter.ListenerAssert<E> )
+        } )
+    
+    else Array( listener ).concat( listeners ).forEach( listener => {
+      const middleware = ( ( ...args: any[] ) => {
+        this.off( event, middleware )
+        return listener?.( ...args )
+      } ) as Emitter.ListenerAssert<E>
+      this.on( event, middleware )
+    } )
+  }
+
+  public on: Emitter.On<E> = <K extends keyof E>(
+    event: K | Emitter.ObjectListeners<E>,
+    listener?: Emitter.ListenerAssert<E, K>,
+    ...listeners: Emitter.ListenerAssert<E, K>[]
+  ) => {
+    if ( typeof event === 'object' ) {
+      const unlisteners = entries( event )
+        .map( ( [ event, listeners ] ) => {
+          if ( Array.isArray( listeners ) ) return this.on( event, listeners[0], ...listeners.slice( 1 ) )
+          return this.on( event, listeners as Emitter.ListenerAssert<E> )
+        } )
+
+      return () => unlisteners.forEach( forEachUnlisteners )
+    }
+
+    if ( !listener ) return () => {}
+
+    this.listeners( event ).push( listener, ...listeners )
+
+    return () => this.off( event, listener, ...listeners )
+  }
+
+  public off: Emitter.Off<E> = <K extends keyof E>(
+    event: K,
+    listener?: Emitter.ListenerAssert<E, K>,
+    ...anothers: Emitter.ListenerAssert<E, K>[]
+  ) => {
+    if ( !listener ) this.__events[event] = []
+    else {
+      const listeners = Array( listener ).concat( anothers )
+      this.__events[event] = this.listeners( event ).filter( listener => !listeners.includes( listener ) )
+    }
+  }
+
+  public emit = <K extends keyof E>( event: K, ...args: Emitter.ListenerArgs<E, K> ) => {
+    this.listeners( event ).forEach( listener => listener( ...args ) )
+  }
+}
+
+namespace Emitter {
+  export type ListenerArgs<E, K extends keyof E> = Parameters<ListenerAssert<E, K>>
+
+  export type ObjectListeners<E> = {
+    [K in keyof E]?: ListenerAssert<E, K> | ListenerAssert<E, K>[]
+  }
+  export type ListenerAssert<E, K extends keyof E = keyof E> = E[K] extends ( ...args: any[] ) => any ? E[K] : never
+  export interface Unlinester {
+    (): void
+  }
+  export interface Off<E> {
+    <K extends keyof E>( event: K, listener: ListenerAssert<E, K>, ...listeners: ListenerAssert<E, K>[] ): void
+    <K extends keyof E>( event: K ): void
+  }
+  export interface On<E> {
+    <K extends keyof E>( event: K, listener: ListenerAssert<E, K>, ...listeners: ListenerAssert<E, K>[] ): Unlinester
+    ( listeners: ObjectListeners<E> ): Unlinester
+  }
+  export interface Once<E> {
+    <K extends keyof E>( event: K, listener: ListenerAssert<E, K>, ...listeners: ListenerAssert<E, K>[] ): void
+    ( listeners: ObjectListeners<E> ): void
+  }
+  export type Events<E> = {
+    [K in keyof E]?: ( E[K] extends ( ...args: any[] ) => any ? E[K] : never )[]
+  }
+}
+
+export default Emitter
